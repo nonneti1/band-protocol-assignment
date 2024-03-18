fef5b6a04c9f9c5a32be5a54af8fcca8ec0c4be9
Netiphong <netiphong.a@giantwhalestudio.com>
2024-03-18 15:50:53 +0700

    docs: Added an explanation for problem_2

--- src/problem-2/main.go
+++ src/problem-2/main.go
@@ -1,20 +1,36 @@
+/*
+* Explanation:
+* This problem_2 function act as a calculator to find how many chickens can superman protected by using a roof.
+
+	Comments:
+	- I've spent too much time on thinking the solution but in the end I'm satisfied by using only one loop.
+
+	Time Complexity: O(n)
+	Memory Complexity: O(1)
+*/
 package main
 
 import "fmt"
 
 func problem_2(roofLength int, chickenPositions []int) int {
+	// Declare a tracker variable for current amount of protectable chicken and current roof position index
 	var currentMaxProtected, currentRoofPosition int
 
+	// Get chicken amount from the length of array
 	chickenAmount := len(chickenPositions)
 
+	// Start by iterating through each chicken
 	for i := 0; i < chickenAmount; i++ {
 		fmt.Println("Index ", i)
 		fmt.Printf("current chicken position : %d ,current roof position %d\n", chickenPositions[i], currentRoofPosition)
 
+		// Check if current chicken can be protected under the roof from current roof position index
 		if chickenPositions[i]-chickenPositions[currentRoofPosition] < roofLength {
+			// Update tracker maximum number of protected chickens by finding the maximum value between currentMaxProtected and i-currentRoofPosition to find how far away the chicken is from the roof position and add 1 because the chicken at currentRoofPosition is also being protected
 			currentMaxProtected = max(currentMaxProtected, i-currentRoofPosition+1)
 			fmt.Println("Update max protected", currentMaxProtected)
 		} else {
+			// Move the roof to the next index position
 			currentRoofPosition++
 			fmt.Println("update roof position", currentRoofPosition)
 		}
@@ -23,6 +39,7 @@ func problem_2(roofLength int, chickenPositions []int) int {
 	return currentMaxProtected
 }
 
+// Find the maximum of two integers.
 func max(a, b int) int {
 	if a > b {
 		return a
